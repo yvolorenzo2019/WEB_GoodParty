954704023ba09a8c1822bfea3b81c74c2c004467
Yvo Lorenzo <47569750+yvolorenzo2019@users.noreply.github.com>
2019-11-03 22:31:14 -0300

    logica de comparação passada paga o ajax do app

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+    session_start();
  try { 
 
  
@@ -11,42 +11,23 @@ session_start();
  $senhaCriptografada = md5($senha);
  
  /* VERIFICANDO SE O USUARIO E SENHA REALMENTE EXISTEM */
-  $query=" SELECT 1 as logado FROM tb_usuario WHERE email = '$email' AND senha = '$senhaCriptografada'";
-        
+  $query=" SELECT cd_usuario ,nivel , email , senha FROM tb_usuario  WHERE email = '$email' AND senha = '$senhaCriptografada';";
+       
     $resultado = mysqli_query($conecta,$query);
-    
-    $logado = "0";
-    while($row = $resultado->fetch_assoc()) {
-       $logado = $row['logado'];
+     
+    if (mysqli_num_rows  ($resultado) > 0){
+     
+       $dados = mysqli_fetch_assoc($resultado);
+       $usuario = array(
+            'cd_usuario' => $dados['cd_usuario'],
+             'email' => $dados['email'],
+              'senha' => $dados['senha'],
+                'nivel' => $dados['nivel'],
+           );
+         
+          
+         echo json_encode($usuario);
     }
-    /* CASO O USUARIO EXISTA O LOGIN SERA IGUAL A 1  E SERA FEITA A VERIFICAÇÃO SE ELE É CLIENTE OU VENDEDOR */
-    if($logado == "1")
-    {
-       $queryVerificaVendedor = " SELECT 1 as cliente FROM tb_usuario WHERE email = '$email' AND senha = '$senhaCriptografada' AND nivel = 'Comprador' ";
-       
-
-       $resultado2 = mysqli_query($conecta,$queryVerificaVendedor);
-       
-       while($row = $resultado2->fetch_assoc()) {
-       $cliente = $row['cliente'];
-       
-       /* SE CLIENTE FOR IGUAL A 1 ELE SERA COMPRADOR , ENTÃO SERA = 1 E VAI SER DIRECIONADO PARA PAGINA DE COMPRADOR */
-        if($cliente == "1"){
-          echo "Bem vindo de volta !" ;
-        }
-        /* SE CLIENTE FOR DIFERENTE DE 1 ELE SERA VENDEDOR , ENTÃO SERA = 1 E VAI SER DIRECIONADO PARA PAGINA DE VENDEDOR */
-          else{
-               echo "2";
-          }
-        }  
-       
-      /* CASO O  LOGIN DE DIFERENTE DE 1 ELE NÃO EXISTIRA */
-    } else {
-
-        echo "Usuario Não Existe :/";
-    }
-    
-    $linhas = mysqli_num_rows($resultado);
   
      }catch (Exception $e ) {
         echo "0";
