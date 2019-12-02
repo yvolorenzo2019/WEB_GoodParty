86114d90d92c5f11402b537bf7fbbca986aa4a2d
Yvo Lorenzo <47569750+yvolorenzo2019@users.noreply.github.com>
2019-12-02 14:03:00 -0300

    Reduzido
    ListarPerfilVendedor.php & ListarPerfilCliente.php Substituidos por listarPerfil,php

---
+++ listaPerfil.php
@@ -0,0 +1,31 @@
+<?php
+           session_start();
+           $codigoS = $_SESSION['cd_usuario'];
+    try {
+       $conecta = mysqli_connect("localhost","id10375079_goodpartybd","AAAAAA","id10375079_goodparty");
+                                //servidor , usuario banco, senha, nome do banco
+    
+        
+       //Consultando banco de dados
+    $query =  "SELECT cd_usuario,nm_usuario,url FROM tb_usuario WHERE cd_usuario = $codigoS";   
+    $resultado = mysqli_query($conecta, $query);
+    $registro = array(
+       'pessoa'=>array()
+    );  
+    $i = 0;
+    while($linha = mysqli_fetch_assoc($resultado)){
+      $registro['pessoa'][$i] = array(
+        'codigo' => $linha['cd_usuario'],
+        'nome' => $linha['nm_usuario'],
+        'foto' => $linha['url'],
+      );
+      $i++;
+    }
+    
+    //Passando vetor em forma de json
+    echo json_encode($registro);
+ 
+    } catch (Exception $e ) {
+        echo "Erro ao buscar: ".$e;
+    }
+?>
